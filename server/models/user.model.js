c014d5eb0bd68ab6ecca56407c671c961cc046fd
arturomrtn <moreton.arturo@gmail.com>
2022-08-01 09:26:15 +0200

    finished setup & starting models

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,1 +1,30 @@
-// User model development
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+	{
+		firstname: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		
+	},
+	{
+		timestamps: true,
+	}
+);
+
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
