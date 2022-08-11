5afc909c5e468d981c1691c3b01992c778243a18
arturomrtn <moreton.arturo@gmail.com>
2022-08-11 19:56:47 +0200

    receiving info

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -4,6 +4,7 @@ const { useState } = React
 
 const Searchbar = () => {
 const [search, setSearch] = useState("")
+const [pokemon, setPokemon] = useState()
 
 const changeText = (e) => {
       setSearch(e.target.value)
@@ -11,20 +12,28 @@ const changeText = (e) => {
 
 const onClick = async (e) => {
       const data = await searchPokemon(search)
-      console.log(data)
-}
-
+      setPokemon(data)
+};
     return (
+
         <div>
-        <div>
+            <div>
             <input placeholder= "Search Pokemon..."
                    onChange = {changeText}/>
+                   </div>
 
         <div>
-            <button onClick = {onClick}/>
+            <button onClick = {onClick} />
+            <div>
+        </div>
+        {pokemon && <div>
+        <div><h1>Nombre: {pokemon?.name}</h1></div>
+        <h1>Peso: {pokemon?.weight}</h1>
+        <div><img src={pokemon.sprites.front_default} alt = "poke-api-pok"/></div>
+        </div>}
         </div>
         </div>
-        </div>
+
     
     );
 }
